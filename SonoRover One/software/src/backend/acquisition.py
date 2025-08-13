3978fee6a4e0953e69e90344be6189fdde1636ff
fusEngineers <148349859+fusEngineers@users.noreply.github.com>
2025-08-13 14:35:16 +0200

    indent bug

--- SonoRover One/software/src/backend/acquisition.py
+++ SonoRover One/software/src/backend/acquisition.py
@@ -711,22 +711,22 @@ class Acquisition:
             params['Sequence']['Unknown power unit'] = str(self.sequence.power_value)
 
         if self.input_param.driving_sys.require_conv_eq:
-           params['Sequence']['volt_curve'] = ("- Voltage to amplitude conversion: Using " +
-                                                "piecewise polynomial fit of " +
-                                                f"{self.sequence.volt_curve_file}\n ")
+            params['Sequence']['volt_curve'] = ("- Voltage to amplitude conversion: Using " +
+                                                 "piecewise polynomial fit of " +
+                                                 f"{self.sequence.volt_curve_file}\n ")
 
             params['Sequence']['power_curve'] = ("- Pressure to amplitude conversion: Using " +
                                                  "piecewise polynomial fit of " +
                                                  f"{self.sequence.power_curve_file}\n ")
-
+            
             params['Sequence']['focus_curve'] = ("- Focus conversion: Using piecewise " +
                                                  "polynomial fit of " +
                                                  f"{self.sequence.focus_curve_file}\n ")
-
+            
             params['Sequence']['eq_curve'] = ("- Normalization factor calculation: Using " +
                                               "piecewise polynomial fit of " +
                                               f"{self.sequence.eq_curve_file}\n ")
-
+            
             params['Sequence']["Normalized pressure [-] based on chosen focal depth wrt exit " +
                                f"plane of {self.sequence._focus_wrt_exit_plane} [mm]"] = (
                                f"{self.sequence._eq_factor} \n ")
